3bf2fde048770a9ff9f989785164050ce21da4db
Natzgun <itzlilouxx@gmail.com>
2024-05-30 01:19:18 -0500

    Nuevo menu de opciones

--- main.cpp
+++ main.cpp
@@ -1,26 +1,49 @@
 #include <iostream>
 
-#include "include/BufferPoolManager.h"
+#include "include/BufferManager.h"
 #include "include/Page.h"
 
+/* Erick Malcoaccha*/
 int main() {
-  BufferPoolManager BfManger;
+  BufferManager BfManger;
 
   while (true) {
     int instruction;
-    std::cout << "1. Load page " << endl;
-    std::cout << "2. Read a page " << endl;
-    std::cout << "3. Update a page " << endl;
-    std::cout << "4. Save page " << endl;
-    std::cout << "5. Exit" << endl;
-    std::cin >> instruction;
+    cout << "================================================================"
+         << endl;
+    cout << "1. Load page " << endl;
+    cout << "2. UpdatePage " << endl;
+    cout << "3. Kill Process " << endl;
+    cout << "4. Save page " << endl;
+    cout << "5. Exit" << endl;
+    cout << "================================================================"
+         << endl;
+    cin >> instruction;
 
     if (instruction == 5) {
       break;
     } else if (instruction == 1) {
-      BfManger.loadPageFromDisk("bloque1");
+      cout << "En que modo desea cargar" << endl;
+      cout << "(L) Lectura" << endl;
+      cout << "(W) Escritura" << endl;
+      char mode;
+      cout << "Mode: ";
+      cin >> mode;
+      // BfManger.loadPageFromDisk("bloque1");
+    } else if (instruction == 2) {
+      // BfManger.updatePage();
     } else if (instruction == 3) {
-      BfManger.updatePage();
+      cout << "Indica el proceso que deseas terminar" << endl;
+      int pageid;
+      cout << "Page ID: ";
+      cin >> pageid;
+    } else if (instruction == 4) {
+      cout << "Indica la pagina que deseas guardar" << endl;
+      /* Aqui deberia retornarme un error si la pagina tiene procesos sin
+       * terminar*/
+      int pageid;
+      cout << "Page ID: ";
+      cin >> pageid;
     }
   }
   return 0;
